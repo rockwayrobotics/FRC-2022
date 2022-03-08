f1b88098dd5980bdc10a08346fdd957be967cef4
IroNinja5 <colinvandermeer@gmail.com>
2022-03-08 18:57:53 -0500

    Change distance to 5 rotations and properly comment code

--- src/main/java/frc/robot/commands/DriveDistance.java
+++ src/main/java/frc/robot/commands/DriveDistance.java
@@ -1,6 +1,3 @@
-// This is "Following" the code from https://frcteam3255.github.io/FRC-Java-Tutorial/programming/autonomous.html but that is really outdated so you have to recode everything
-// This will also not work until it is added ti RobotContainer under Command getAutonomousCommand() so that yourself becuase I don't know how to do that
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -8,15 +5,6 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivebaseSubsystem;
 
 
-/**
-
- * A simple command that grabs a hatch with the {@link HatchSubsystem}. Written explicitly for
-
- * pedagogical purposes. Actual code should inline a command this simple with {@link
-
- * edu.wpi.first.wpilibj2.command.InstantCommand}.
-
- */
 
 public class DriveDistance extends CommandBase {
 
@@ -26,7 +14,14 @@ public class DriveDistance extends CommandBase {
 
   private double m_distance;
   private double m_speed;
-
+  
+  /**
+   * A command that spins the wheels for a certain distance
+   * @param subsystem Set this to m_drivebase
+   * @param distance Set to distance in inches
+   * @param speed Set to speed from zero to one
+  
+   */
   public DriveDistance(DrivebaseSubsystem subsystem, double distance, double speed) {
 
     m_drivebase = subsystem;
@@ -42,28 +37,26 @@ public class DriveDistance extends CommandBase {
   public void initialize() {
     // Resets encoder values to default
     m_drivebase.resetEncoders();
-
   }
 
   @Override
   public void execute() {
-    // This needs to be constantly forward
+    // Sets the drivebase to go forward from the speed variable
     m_drivebase.set(0, m_speed, 1);
-
   }
 
 
   @Override
   public boolean isFinished() {
-    // This will say to end when the encoders are equal with the distance we want
+    // This takes the values from encoder L and R and averages them out
     double averageDistance = (m_drivebase.getLDistance() + m_drivebase.getRDistance()) / 2; 
-    return averageDistance >= m_distance;  //there are encoders for l and r
-
+    // This will say to end when the encoders are equal with the distance we want
+    return averageDistance >= m_distance;  
   }
 
   @Override
   public void end(boolean cancelled) {
-    m_drivebase.set(0,0,0);
+    m_drivebase.set(0,0,0); // Resets the drivebase to 0
   }
 
 }
