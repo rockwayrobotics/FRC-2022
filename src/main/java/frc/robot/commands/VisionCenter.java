0ffca00570c1be9a5d68f003d32c1ace71a9a7da
IroNinja5 <colinvandermeer@gmail.com>
2022-03-18 19:38:28 -0400

    Add network table code that might work

--- src/main/java/frc/robot/commands/VisionCenter.java
+++ src/main/java/frc/robot/commands/VisionCenter.java
@@ -3,7 +3,14 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivebaseSubsystem;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
 public class VisionCenter extends CommandBase {
+
+  NetworkTableEntry camEntry;
+
   private DrivebaseSubsystem m_drivebase;
   private final int LEFT = 0;
   private final int RIGHT = 1; 
@@ -19,12 +26,24 @@ public class VisionCenter extends CommandBase {
   @Override
   public void initialize() {
     m_stop = false;
+          //Get the default instance of NetworkTables that was created automatically
+      //when your program starts
+      NetworkTableInstance inst = NetworkTableInstance.getDefault();
+
+      //Get the table within that instance that contains the data. There can
+      //be as many tables as you like and exist to make it easier to organize
+      //your data. In this case, it's a table called datatable.
+      NetworkTable table = inst.getTable("datatable");
+
+      //Get the entries within that table that correspond to the X and Y values
+      //for some operation in your program.
+      camEntry = table.getEntry("CAM");
   }
 
   @Override
   public void execute() {
-    int command = LEFT; 
-
+    int command = camEntry.getNumber(2).intValue(); 
+    
     if (command == LEFT) {
       m_drivebase.set(-0.5, 0);
     }
